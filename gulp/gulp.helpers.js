26855ca8002610c4864e2ce23fe77bd3ddd3ac31
Simon Coope <simon.coope@sjcnet.co.uk>
2015-02-19 10:35:54 +0000

    Build Process Updates

--- gulp/gulp.helpers.js
+++ gulp/gulp.helpers.js
@@ -1,25 +1,86 @@
 module.exports = function () {
     'use strict';
 
+    // Dependencies
     var util = require('gulp-util');
+    var del = require('del');
+    var nodemon = require('gulp-nodemon');
+    var browserSync = require('browser-sync');
+    var config = require('./gulp.config')();
+
+    // TODO: Put somewhere else.
+    function log(msg) {
+        if (typeof(msg) === 'object') {
+            for (var item in msg) {
+                if (msg.hasOwnProperty(item)) {
+                    util.log(util.colors.green(msg[item]));
+                }
+            }
+        } else {
+            util.log(util.colors.green(msg));
+        }
+    }
 
     var helpers = {
-        log: function(msg) {
-            if (typeof(msg) === 'object') {
-                for (var item in msg) {
-                    if (msg.hasOwnProperty(item)) {
-                        util.log(util.colors.green(msg[item]));
-                    }
-                }
-            } else {
-                util.log(util.colors.green(msg));
-            }
+        clean: function (path) {
+            del(path, {force: true});
         },
         errorLogger: function (error) {
             util.log(util.colors.red('*** Start of Error ***'));
             util.log(util.colors.red(error));
             util.log(util.colors.red('*** End of Error ***'));
             this.emit('end');
+        },
+        log: log,
+        serve: function (isDev) {
+            return nodemon({
+                script: config.nodeServer,
+                delayTime: 1,
+                env: {
+                    'PORT': config.defaultPort,
+                    'NODE_ENV': isDev ? 'dev' : 'build'
+                },
+                watch: config.server
+            })
+                .on('restart', ['analyse'], function (ev) {
+                    log('*** nodemon restarted');
+                    log('files changed on restart:\n' + ev);
+                })
+                .on('start', function () {
+                    log('*** nodemon started');
+
+                    if (browserSync.active) {
+                        return;
+                    }
+
+                    var options = {
+                        proxy: 'localhost:' + config.defaultPort,
+                        port: 3000,
+                        files: [
+                            './src/**/*.*'
+                        ],
+                        ghostMode: {
+                            clicks: true,
+                            location: false,
+                            forms: true,
+                            scroll: true
+                        },
+                        injectChanges: true,
+                        logFileChanges: true,
+                        logLevel: 'debug',
+                        logPrefix: 'recipeManager',
+                        notify: true,
+                        reloadDelay: 0 //1000
+                    };
+
+                    browserSync(options);
+                })
+                .on('crash', function () {
+                    log('*** nodemon crashed!!');
+                })
+                .on('exit', function () {
+                    log('*** nodemon exited');
+                });
         }
     };
 
